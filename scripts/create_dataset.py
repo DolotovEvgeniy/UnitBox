d17051c92bdb34e469de1ba0a0da562631b64bc5
DolotovEveniy <dolotov.evgeniy@gmail.com>
2016-10-30 00:30:27 +0300

    resize dataset image

--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -13,6 +13,12 @@ class Ellipse:
         self.center_x_ = center_x
         self.center_y_ = center_y
     
+    def scale(self, scaleCoeff) :
+        self.major_axis_radius_ *= scaleCoeff
+        self.minor_axis_radius_ *= scaleCoeff
+        self.center_x_ *= scaleCoeff
+        self.center_y_ *= scaleCoeff
+
     def toRect(self) :
         alpha = atan(-self.major_axis_radius_*tan(self.angle_)/self.minor_axis_radius_)
         betta = atan(self.major_axis_radius_/(tan(self.angle_)*self.minor_axis_radius_))
@@ -27,14 +33,15 @@ class Ellipse:
         xSide = fabs(xMax-xMin);
         ySide = fabs(yMin-yMax);
         return (int(self.center_x_-ySide/2.0),int(self.center_y_-xSide/2.0), int(ySide), int(xSide))
+    
+    def draw(self, image, color, thickness) :
+        cv2.ellipse(image, (int(self.center_x_), int(self.center_y_)), (int(self.major_axis_radius_), int(self.minor_axis_radius_)), 180*self.angle_/3.14, 0, 360, color, thickness)
+    
     major_axis_radius_ = 0
     minor_axis_radius_ = 0
     angle_ = 0
     center_x_ = 0
     center_y_ = 0
-    
-    def draw(self, image, color, thickness) :
-        cv2.ellipse(image, (int(self.center_x_), int(self.center_y_)), (int(self.major_axis_radius_), int(self.minor_axis_radius_)), 180*self.angle_/3.14, 0, 360, color, thickness)
 
 
 def readEllipse(f) :
@@ -53,8 +60,12 @@ def distanceToRect(rect, point) :
         return (point[0]-rect[0], point[1]-rect[1], rect[0]+rect[2]-point[0], rect[1]+rect[3]-point[1])
     else :
         return (0, 0, 0, 0)
+
+
 fddb_file = sys.argv[1] 
 prefix = sys.argv[2]
+data_directory = sys.argv[3]
+sampleSize = int(sys.argv[4])
 f = open(fddb_file, 'r')
 fileList = []
 index = 1
@@ -64,12 +75,33 @@ while True:
         break
     print img_path
     image = cv2.imread(img_path)
+    cv2.imwrite("img1.png", image)
+    scale = 0
+    oldSize = (image.shape[1], image.shape[0])
+    newSize = 0
+    if image.shape[0] < image.shape[1] :
+        scale = float(sampleSize)/image.shape[0]
+        newSize = (int(image.shape[1]*scale), sampleSize)
+    else :
+        scale = float(sampleSize)/image.shape[1]
+        newSize = (sampleSize, int(image.shape[0]*scale))
+    print "resize: ", oldSize, "->", newSize
+    print scale
+    image = cv2.resize(image, newSize)
+    cv2.imwrite("img.png", image)
+    images = np.zeros((1, 3, image.shape[0], image.shape[1]), dtype = "uint8")
+    confidence_heatmaps = np.zeros((1, 1, image.shape[0], image.shape[1]), dtype = "uint8")
+    
+    boundingbox_heatmaps = np.zeros((1, 4,image.shape[0], image.shape[1]), dtype = "uint32")
+
     confidence_heatmap = np.zeros((image.shape[0], image.shape[1], 1), dtype = "uint8")
     boundingbox_heatmap = np.zeros((image.shape[0], image.shape[1], 4), dtype = "uint32")
+
     objectCount = int(f.readline())
     rectList = []
     for i in range(0, objectCount):
         newEllipse = readEllipse(f)
+        newEllipse.scale(scale)
         newEllipse.draw(confidence_heatmap, 255, -1)
         r = newEllipse.toRect()
         rectList.append(r)
@@ -82,11 +114,16 @@ while True:
                         boundingbox_heatmap[x,y,1]=dist[1]
                         boundingbox_heatmap[x,y,2]=dist[2]
                         boundingbox_heatmap[x,y,3]=dist[3]
-    filename = 'train%d.h5' % index
+    cv2.imshow("conf", confidence_heatmap)
+    cv2.waitKey(0)
+    confidence_heatmaps[0] = np.transpose(confidence_heatmap, (2, 0, 1))
+    images[0] = np.transpose(image, (2, 0, 1))
+    boundingbox_heatmaps[0] = np.transpose(boundingbox_heatmap, (2, 0, 1))
+    filename = sys.argv[3]+'train%d.h5' % index
     h = h5py.File(filename, 'w') 
-    h['image'] = image
-    h['confidence'] = confidence_heatmap
-    h['boundingbox']= boundingbox_heatmap
+    h['image'] = images
+    h['confidence'] = confidence_heatmaps
+    h['boundingbox']= boundingbox_heatmaps
     fileList.append(filename)
     index+=1
     h.close()
