b999a3ffe92c49670ea836f1ca69cc0f0a0b9778
DolotovEvgeniy <dolotov.evgeniy@gmail.com>
2017-02-07 23:15:38 +0300

    complete class BoundingBoxMap

--- include/boundingbox_map.h
+++ include/boundingbox_map.h
@@ -3,24 +3,37 @@
 #ifndef BOUNDINGBOX_MAP_H
 #define BOUNDINGBOX_MAP_H
 
+#include <array>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+enum BoundingboxMapChannel {
+    TOP   = 0,
+    DOWN  = 1,
+    RIGHT = 2,
+    LEFT  = 3
+};
+
+const std::vector<BoundingboxMapChannel> CHANNELS = {TOP, DOWN, RIGHT, LEFT};
+
+#define BOUNDINGBOX_MAP_CHANNEL_COUNT 4
+
 class BoundingboxMap {
 public:
-    BoundingboxMap();
+    BoundingboxMap() = default;
     BoundingboxMap(int width, int height);
     BoundingboxMap(int width, int height, float* data);
     BoundingboxMap(const BoundingboxMap& map);
     BoundingboxMap& operator=(const BoundingboxMap& map);
     cv::Size size() const;
-    int channels() const;
-    float& at(int x, int y, int channel);
-    float at(int x,int y, int channel) const;
+    int channelsCount() const;
+    float& at(int x, int y, BoundingboxMapChannel channel);
+    float at(int x,int y, BoundingboxMapChannel channel) const;
     cv::Rect getRect(int x, int y) const;
 private:
-    cv::Mat mapTop, mapDown, mapRight, mapLeft;
+    std::array<cv::Mat, 4> map_;
 };
 
 #endif // BOUNDINGBOX_MAP_H
--- src/boundingbox_map.cpp
+++ src/boundingbox_map.cpp
@@ -1,48 +1,62 @@
 #include "boundingbox_map.h"
-
+#include <cassert>
 using namespace std;
 using namespace cv;
 
-BoundingboxMap::BoundingboxMap() {
-
-}
-
 BoundingboxMap::BoundingboxMap(int width, int height, float* data) {
-
+    for(int i = 0; channelsCount(); i++) {
+        map_[i] = Mat(height, width, CV_32FC1, data + width*height*i);
+    }
 }
 
 BoundingboxMap::BoundingboxMap(const BoundingboxMap& map) {
-
+    for(auto channel: CHANNELS) {
+        this->map_.at(channel) = map.map_.at(channel);
+    }
 }
 
 BoundingboxMap& BoundingboxMap::operator=(const BoundingboxMap& map) {
-   return *this;
+    for(auto channel: CHANNELS) {
+        this->map_.at(channel) = map.map_.at(channel);
+    }
+    return *this;
 }
 
 Rect BoundingboxMap::getRect(int x, int y) const{
-    return Rect();
+    Point leftTopPoint;
+    leftTopPoint.x = x - map_.at(LEFT).at<float>(Point(x, y));
+    leftTopPoint.y = y - map_.at(TOP).at<float>(Point(x, y));
+
+    int width  = map_.at(LEFT).at<float>(Point(x, y))
+                 + map_.at(RIGHT).at<float>(Point(x, y));
+
+    int height = map_.at(TOP).at<float>(Point(x, y))
+                 + map_.at(DOWN).at<float>(Point(x, y));
+    return Rect(leftTopPoint, Size(width, height));
 }
 
-float BoundingboxMap::at(int x, int y, int channel) const {
-    return 0;
+float BoundingboxMap::at(int x, int y, BoundingboxMapChannel channel) const {
+    assert(x >= 0 && y >= 0 && x < size().width && y < size().height);
+
+    return map_.at(channel).at<float>(Point(x, y));
 }
 
-float& BoundingboxMap::at(int x, int y, int channel) {
-    float i;
-    return i;
+float& BoundingboxMap::at(int x, int y, BoundingboxMapChannel channel) {
+    assert(x >= 0 && y >= 0 && x < size().width && y < size().height);
+
+    return map_.at(channel).at<float>(Point(x, y));
 }
 
-int BoundingboxMap::channels() const {
-    return 4;
+int BoundingboxMap::channelsCount() const {
+    return map_.size();
 }
 
 Size BoundingboxMap::size() const {
-    return Size(mapDown.cols, mapDown.rows);
+    return Size(map_.at(0).cols, map_.at(0).rows);
 }
 
 BoundingboxMap::BoundingboxMap(int width, int height) {
-    mapDown = Mat::zeros(height, width, CV_32FC1);
-    mapTop =  Mat::zeros(height, width, CV_32FC1);
-    mapRight =  Mat::zeros(height, width, CV_32FC1);
-    mapLeft =  Mat::zeros(height, width, CV_32FC1);
+    for(auto& channel: map_) {
+        channel = Mat::zeros(height, width, CV_32FC1);
+    }
 }
